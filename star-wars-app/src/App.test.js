9931444dd978c40e5058c701103ff7f694b8a67f
CharloDevERA <c.best-zambrana@erasolutions.us>
2023-10-18 12:35:13 -0400

    added App.test.js

---
+++ star-wars-app/src/App.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import App from './App';
+
+test('Making sure that the starwars logo renders on screen', () => {
+  const wrapper = rtl.render(<App />)
+  const logoImg = wrapper.getByAltText(/^logo$/i)
+  expect(logoImg).toBeInTheDocument()
+})
+
+test('Making sure buttons render on screen ')
